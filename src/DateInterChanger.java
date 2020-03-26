8782b3c942bb7e3bf71527a5ac4c671a333e8bdd
Cameron Doherty <5cdoherty18@sau16.org>
2020-03-26 13:18:47 -0400

    Basic Structure for project done

--- src/DateInterChanger.java
+++ src/DateInterChanger.java
@@ -1,4 +1,52 @@
+import java.util.Scanner;
 //Project 9 Cameron Doherty
 public class DateInterChanger {
+    int year;
+    int month;
+    int day;
 
+    public DateInterChanger(String date, char type) {
+    //Extracting from date day month and year based on the char type
+    }
+
+    public String getISODate() {
+        return null; //change when ready
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String userDate;
+        char dateType;
+        //Build prompt to ask user for format "C / M"
+        //Get input from user c or m
+        //Build prompt to ask for date
+        //Get date input
+        //Create dateInterChanger obj
+        //Output isodate for the obj
+    }
 }
